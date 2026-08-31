04a1c0eaf88f45d405c55771c286d5456d3492e0
phirrehan <phirrehan@gmail.com>
2026-08-31 18:06:11 +0500

    used keyd for swapping capslock and escape key

--- .config/caelestia/hypr/input.lua
+++ .config/caelestia/hypr/input.lua
@@ -1,7 +1,8 @@
 hl.config({
 	input = {
 		kb_layout = "us,pk",
-		kb_options = "grp:alt_altgr_toggle,caps:swapescape",
+		-- kb_options = "grp:alt_altgr_toggle,caps:swapescape",
+		kb_options = "grp:alt_altgr_toggle",
 		sensitivity = 0.8,
 	},
 })
