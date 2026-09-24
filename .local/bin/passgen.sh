edfe21377b3ebb166e6eb48cdc5c636f703f1c81
phirrehan <phirrehan@gmail.com>
2026-09-24 20:59:45 +0500

    updated passgen.sh to organize passwords after generation

--- .local/bin/passgen.sh
+++ .local/bin/passgen.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+footConfigDir="$HOME/.config/foot"
+
 # input
-foot -a input -c ~/.config/foot/input.ini -e bash -c '
+foot -a input -c "$footConfigDir/input.ini" -e bash -c '
 read -p "Enter name of password: " passName
 [ -z "$passName" ] && exit 1
 read -p "Enter length of password: " passLength
@@ -10,16 +12,19 @@ if [[ ! "$passLength" =~ ^[0-9]+$ ]]; then
   echo "Error: Expected a (positive) Number"
   read -p "Press enter to exit." && exit 3
 fi
+
 printf "%s" "$passName">"$XDG_RUNTIME_DIR/pass-name"
 printf "%s" "$passLength">"$XDG_RUNTIME_DIR/pass-length"
 '
+exitCode=$?
+[ $exitCode = 0 ] || exit $exitCode
 
 # variables
 tmpName="$XDG_RUNTIME_DIR/pass-name"
 tmpLength="$XDG_RUNTIME_DIR/pass-length"
-[ -f "$tmpName" ] && [ -f "$tmpLength" ] || exit 1
 passName=$(cat "$tmpName")
 passLength=$(cat "$tmpLength")
+passDir="$(ls "$PASSWORD_STORE_DIR" | fuzzel --dmenu)"
 
 # generate password and type it after removing
 # ansi esc sequences and newline characters from text
@@ -27,5 +32,8 @@ pass generate -f "$passName" "$passLength" |
   awk 'NR==2 { gsub(/\x1B\[[0-9;]*[[:alpha:]]/, ""); printf "%s", $0 }' |
   wtype -
 
-# delete temporary variables
+# move the password to it's appropriate directory
+pass mv "$passName" "$passDir/"
+
+# delete temporary files
 rm "$tmpName" "$tmpLength"
